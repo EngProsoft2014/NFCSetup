2c27e7839e5e818945ac402d8f4f162dbc13851b
AhmedFayez299 <93268385+AhmedFayez299@users.noreply.github.com>
2024-11-28 15:32:38 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NFCSetup
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());  
+        }
+    }
+}
