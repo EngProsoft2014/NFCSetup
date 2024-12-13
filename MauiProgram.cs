3439dd5fd7a6376e72753a5e918ad64cd0e2589a
MR.Akl <MR.Akl@DESKTOP-01O5FDQ>
2024-12-14 00:38:27 +0200

    update

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -22,6 +22,7 @@ namespace NFCSetup
                 .ConfigureMopups()
                 .ConfigureSyncfusionCore()
                 .UseSkiaSharp()
+                
                 .UseBarcodeReader()
                 .ConfigureFonts(fonts =>
                 {
